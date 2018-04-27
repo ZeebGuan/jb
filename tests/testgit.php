8261126809bc8c66653807b1a14598f9380ced73
zeeb <794081329@qq.com>
2018-04-27 17:35:33 +0800

    默认的

---
+++ tests/testgit.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/27
+ * Time: 16:11
+ */
+namespace tests;
+
+class testgit
+{
+    function func(){
+        echo "hello";
+    }
+
+}
